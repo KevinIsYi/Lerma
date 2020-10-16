dd31adc903a353d48b69f6d155ee18b8692f0612
Kevin Rodriguez <rodriguezkevin95g@gmail.com>
2020-10-16 12:17:13 -0500

    Added animation when click on menu bars

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const menuBars = document.getElementById('mobile-menu-bars');
+const mobileMenu = document.getElementById('mobile-menu');
+const closeMobileMenuIcon = document.getElementById('close-mobile-menu');
+
+const clickRemoveMenu = () => {
+    mobileMenu.style.display = 'none';
+    closeMobileMenuIcon.removeEventListener('click', clickRemoveMenu);
+}
+
+const eventListeners = () => {
+    menuBars.addEventListener('click', () => {
+        mobileMenu.style.display = 'flex';
+        closeMobileMenuIcon.addEventListener('click', clickRemoveMenu);
+    });
+}
+
+eventListeners();
