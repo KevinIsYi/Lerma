99d337704a1bcf22d98046cf7b7eb1f5f291cf81
Kevin Rodriguez <rodriguezkevin95g@gmail.com>
2020-10-17 19:17:43 -0500

    Added function that reads a json

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+/*
 const menuBars = document.getElementById('mobile-menu-bars');
 const mobileMenu = document.getElementById('mobile-menu');
 const closeMobileMenuIcon = document.getElementById('close-mobile-menu');
@@ -15,3 +16,22 @@ const eventListeners = () => {
 }
 
 eventListeners();
+*/
+
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+readTextFile("lerma.json", function(text){
+    var data = JSON.parse(text);
+    console.log(data);
+
+});
